b74e95ad66ecc44afba8aafc3ec67ef9140dbb94
Daniel Weiß <daniel.weiss@grollmus.de>
2025-04-12 07:52:17 +0200

    add sdk methods to signalize azure devops that the extension loads correctly #5

--- src/common.tsx
+++ src/common.tsx
@@ -1,9 +1,15 @@
 import "azure-devops-ui/Core/override.css";
 import "es6-promise/auto";
+import * as SDK from "azure-devops-extension-sdk";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./common.scss";
 
+SDK.init();
+
 export function showRootComponent(component: React.ReactElement<any>) {
-    ReactDOM.render(component, document.getElementById("root"));
+    ReactDOM.render(component, document.getElementById("root"), () => {
+        console.log("✅ React component mounted");
+        SDK.ready();
+    });
 }
